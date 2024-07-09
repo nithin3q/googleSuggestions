1a2fbe1d176daef92e63abc7d06c72f338d502e0
nithin3q <52022957+nithin3q@users.noreply.github.com>
2024-07-09 22:02:46 +0530

    cors

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
 import './App.css';
-import React,{Component} from 'react'
-import axios from 'axios'
-import ItemsEach from './ItemsEach/'
+import React, { useState } from 'react';
+import axios from 'axios';
+import ItemsEach from './ItemsEach';
 
-class App extends Component{
-  state = {search_result: '', suggestions: []}
-  getSearch = event =>{
-    this.setState({search_result: event.target.value})
-  }
-  getSuggestions = async() =>{
-    const {search_result} = this.state
-    try{
-      const response = await axios.get('/complete/search', {
+const App = () => {
+  const [searchResult, setSearchResult] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+  const getSearch = (event) => {
+    setSearchResult(event.target.value);
+  };
+
+  const getSuggestions = async () => {
+    try {
+      const response = await axios.get(`/complete/search`, {
         params: {
           client: 'firefox',
-          q: search_result
+          q: searchResult,
         },
-      })
-      this.setState({suggestions: response.data[1]})
+      });
+      setSuggestions(response.data[1]);
+    } catch (e) {
+      console.log(e);
     }
-    catch(e){
-      console.log(e)
-    }
-  }
-  render(){
-    const {suggestions} = this.state
-    return(
-      <div className = 'main-background'>
-        <img src = 'https://assets.ccbp.in/frontend/react-js/google-logo.png' alt = 'logo google' className=  'google-image'/>
-        <br/>
-        <input type = 'search' placeholder = 'Enter that what do you want to search' onChange = {this.getSearch} className = 'input-box'/>
-        <button onClick = {this.getSuggestions} className = 'button'>submit</button>
-        
-        <ul>
-          {suggestions.map((eachItem, index) =>(
-            <ItemsEach key = {index} item = {eachItem}/>
-          ))}
-        </ul>
-      </div>
-    )
-  }
-}
+  };
+
+  return (
+    <div className="main-background">
+      <img src="https://assets.ccbp.in/frontend/react-js/google-logo.png" alt="logo google" className="google-image" />
+      <br />
+      <input
+        type="search"
+        placeholder="Enter what you want to search"
+        onChange={getSearch}
+        className="input-box"
+      />
+      <button onClick={getSuggestions} className="button">
+        Submit
+      </button>
+
+      <ul>
+        {suggestions.map((eachItem, index) => (
+          <ItemsEach key={index} item={eachItem} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
